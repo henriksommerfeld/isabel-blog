c815a78b49ddaa790601780289a141ec0dc5f0eb
Henrik Sommerfeld <henrik@sommerfeld.nu>
2019-08-08 20:46:14 +0200

    404 JS => TSX

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function NotFoundPage() {
+  return (
+    <Layout>
+      <div>
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn't exist... the sadness.</p>
+      </div>
+    </Layout>
+  );
+}
