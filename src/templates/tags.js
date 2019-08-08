a3b2a835f0af80103067e83403fc4d904673116b
Henrik Sommerfeld <henrik@sommerfeld.nu>
2019-08-08 06:38:23 +0200

    Improved tags.js

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,52 +1,49 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import { Link, graphql } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 import Layout from '../components/Layout';
 
-class TagRoute extends React.Component {
-  render() {
-    const posts = this.props.data.allMarkdownRemark.edges;
-    const postLinks = posts.map(post => (
-      <li key={post.node.fields.slug}>
-        <Link to={post.node.fields.slug}>
-          <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
-        </Link>
-      </li>
-    ));
-    const tag = this.props.pageContext.tag;
-    const title = this.props.data.site.siteMetadata.title;
-    const totalCount = this.props.data.allMarkdownRemark.totalCount;
-    const tagHeader = `${totalCount} post${
-      totalCount === 1 ? '' : 's'
-    } tagged with “${tag}”`;
+export default function TagRoute({ pageContext }) {
+  const data = useStaticQuery(tagPageQuery);
+  const posts = data.allMarkdownRemark.edges;
+  const postLinks = posts.map(post => (
+    <li key={post.node.fields.slug}>
+      <Link to={post.node.fields.slug}>
+        <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
+      </Link>
+    </li>
+  ));
+  const tag = pageContext.tag;
+  const title = data.site.siteMetadata.title;
+  const totalCount = data.allMarkdownRemark.totalCount;
+  const tagHeader = `${totalCount} post${
+    totalCount === 1 ? '' : 's'
+  } tagged with “${tag}”`;
 
-    return (
-      <Layout>
-        <section className="section">
-          <Helmet title={`${tag} | ${title}`} />
-          <div className="container content">
-            <div className="columns">
-              <div
-                className="column is-10 is-offset-1"
-                style={{ marginBottom: '6rem' }}
-              >
-                <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
-                <ul className="taglist">{postLinks}</ul>
-                <p>
-                  <Link to="/tags/">Browse all tags</Link>
-                </p>
-              </div>
+  return (
+    <Layout>
+      <section className="section">
+        <Helmet title={`${tag} | ${title}`} />
+        <div className="container content">
+          <div className="columns">
+            <div
+              className="column is-10 is-offset-1"
+              style={{ marginBottom: '6rem' }}
+            >
+              <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
+              <ul className="taglist">{postLinks}</ul>
+              <p>
+                <Link to="/tags/">Browse all tags</Link>
+              </p>
             </div>
           </div>
-        </section>
-      </Layout>
-    );
-  }
+        </div>
+      </section>
+    </Layout>
+  );
 }
 
-export default TagRoute;
-
-export const tagPageQuery = graphql`
+const tagPageQuery = graphql`
   query TagPage($tag: String) {
     site {
       siteMetadata {
