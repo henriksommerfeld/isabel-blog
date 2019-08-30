52da51e00d706109cab31adb480c22ea959a0ad9
Henrik Sommerfeld <henrik@sommerfeld.nu>
2019-08-30 15:06:28 +0200

    Mobile menu works ok

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,42 +2,73 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { colors } from '../variables';
 import { Link } from 'gatsby';
-import { Location } from '@reach/router';
-import { useSpring, animated, config } from 'react-spring';
+import { useSpring, animated } from 'react-spring';
 import HamburgerMenuIcon from './HamburgerMenuIcon';
 import PortraitSmall from './PortraitSmall';
 
 export default function Header() {
   const [mobileMenuIsVisible, setMobileMenuIsVisible] = useState(false);
-  const toggleMenu = () => setMobileMenuIsVisible(x => !x);
+  const toggleMenu = () => setMobileMenuIsVisible((x: boolean) => !x);
   const showPortrait = location.pathname !== '/';
-  const spring = useSpring({
-    from: {
-      transform: mobileMenuIsVisible ? 'translateY(0)' : 'translateY(-100%)',
-    },
+  const pullDownAnimation = useSpring({
     to: {
-      transform: !mobileMenuIsVisible ? 'translateY(0)' : 'translateY(-100%)',
+      transform: mobileMenuIsVisible
+        ? `translateY(${headerHeight})`
+        : `translateY(-${mobileMenuHeight})`,
     },
   });
 
   return (
-    <NavStyled>
-      <HeaderLink to="/" aria-label="till startsidan">
-        {showPortrait ? <PortraitSmall /> : null}
-        <Isabel>Isabel Sommerfeld</Isabel>
-      </HeaderLink>
-      <HamburgerMenuIcon
-        isOpen={mobileMenuIsVisible}
-        clickAction={toggleMenu}
-      />
-      <animated.div style={spring}>
-        <Link to="/om">Om mig</Link>
-        <Link to="/publicerat">Publicerat</Link>
-        <Link to="/blog">Pressbilder</Link>
-      </animated.div>
-    </NavStyled>
+    <>
+      <NavStyled>
+        <HeaderLink to="/" aria-label="till startsidan">
+          {showPortrait ? <PortraitSmall /> : null}
+          <Isabel>Isabel Sommerfeld</Isabel>
+        </HeaderLink>
+        <HamburgerMenuIcon
+          isOpen={mobileMenuIsVisible}
+          clickAction={toggleMenu}
+        />
+      </NavStyled>
+      <MobileMenu style={pullDownAnimation}>
+        <MobileLink to="/om">1</MobileLink>
+        <MobileLink to="/publicerat">2</MobileLink>
+        <MobileLink to="/blog">3</MobileLink>
+        <MobileLink to="/om">4</MobileLink>
+        <MobileLink to="/publicerat">5</MobileLink>
+        <MobileLink to="/blog">6</MobileLink>
+        <MobileLink to="/om">7</MobileLink>
+        <MobileLink to="/publicerat">8</MobileLink>
+        <MobileLink to="/blog">9</MobileLink>
+      </MobileMenu>
+    </>
   );
 }
+
+const headerHeight = '80px';
+const mobileMenuHeight = '500px';
+
+const MobileMenu = styled(animated.div)`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background: white;
+  z-index: 1;
+  position: absolute;
+  right: 0;
+  top: 0;
+  transform: translateY(-${mobileMenuHeight});
+  box-shadow: 0 1px 20px rgba(0, 0, 0, 0.2);
+`;
+
+const MobileLink = styled(Link)`
+  padding: 1rem;
+  text-align: center;
+
+  :hover {
+    background: rgba(0, 0, 0, 0.2);
+  }
+`;
 
 const HeaderLink = styled(Link)`
   display: flex;
@@ -60,5 +91,6 @@ const NavStyled = styled('nav')`
   padding: 1rem;
   background: ${colors.headerBackground};
   box-shadow: 0 1px 20px rgba(0, 0, 0, 0.2);
-  z-index: 1;
+  z-index: 2;
+  height: ${headerHeight};
 `;
