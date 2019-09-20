ccc97892177e96fd9f92c42c5ade35bb63b9d8b8
Henrik Sommerfeld <henrik@sommerfeld.nu>
2019-09-20 15:54:54 +0200

    Searchbox positioning

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,11 +3,12 @@ import styled from 'styled-components';
 import { colors, breakpoints, spacing, layout } from '../constants';
 import { tailwindColors } from '../tailwind-colors';
 import { transparentizeHex } from '../color-convertions';
-import SearchWhiteSvg from '../../static/img/search.svg';
-import SearchRedSvg from '../../static/img/search-red700.svg';
+import SearchWhiteSvg from '../../static/img/search-white.svg';
+import SearchGreySvg from '../../static/img/search-grey100.svg';
 
 export default function Search() {
   const [queryInput, setQueryInput] = useState('');
+  const [hasFocus, setHasFocus] = useState(false);
 
   const searchClicked = () => {
     console.log('Search clicked', queryInput);
@@ -19,9 +20,9 @@ export default function Search() {
 
   return (
     <SearchArea>
-      <SearchBox>
-        <label for="searchbox" className="screen-reader-text">
-          Ange dina sökord här...
+      <SearchBox hasFocus={hasFocus}>
+        <label htmlFor="searchbox" className="screen-reader-text">
+          {`Ange dina sökord här... ${hasFocus}`}
         </label>
         <SearchBoxInput
           id="searchbox"
@@ -29,9 +30,19 @@ export default function Search() {
           role="entry"
           placeholder="Ange dina sökord här..."
           onChange={queryInputChanged}
+          onFocus={() => setHasFocus(true)}
+          onBlur={() => setHasFocus(false)}
         />
-        <SearchButton onClick={searchClicked} aria-label="Sök">
-          <SearchIcon src={SearchRedSvg} aria-hidden />
+        <SearchButton
+          onClick={searchClicked}
+          aria-label="Sök"
+          onFocus={() => setHasFocus(true)}
+          onBlur={() => setHasFocus(false)}
+        >
+          <SearchIcon
+            src={hasFocus ? SearchWhiteSvg : SearchGreySvg}
+            aria-hidden
+          />
         </SearchButton>
       </SearchBox>
     </SearchArea>
@@ -42,6 +53,7 @@ const SearchArea = styled('div')`
   display: flex;
   justify-content: center;
   width: 100%;
+  max-width: ${layout.contentMaxWidth}px;
   padding: 1rem;
 `;
 
@@ -52,23 +64,22 @@ const SearchBox = styled('div')`
   align-items: center;
   padding: 0.5em;
   width: 100%;
-  max-width: ${layout.contentMaxWidth}px;
   border-radius: ${layout.borderRadius};
-  border: ${transparentizeHex(colors.white, 0.7)} solid 1px;
-  /* transition: background-color 150ms ease; */
-  background-color: ${transparentizeHex(colors.white, 0.7)};
+  border-width: 2px;
+  border-style: solid;
+  border-color: ${({ hasFocus }) =>
+    hasFocus ? colors.white : tailwindColors.gray100};
+
+  background-color: ${({ hasFocus }) =>
+    transparentizeHex(colors.white, hasFocus ? 0.7 : 0.6)};
 `;
 
 const SearchBoxInput = styled('input')`
   width: 100%;
   background-color: transparent;
   border: none;
-  caret-color: ${colors.white};
+  caret-color: ${tailwindColors.red700};
   color: ${colors.black};
-
-  ::placeholder {
-    color: ${colors.black};
-  }
 `;
 
 const SearchButton = styled('button')`
