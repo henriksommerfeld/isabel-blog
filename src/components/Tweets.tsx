614d58814ad6207453b2ccd19c9fae6f246c29c4
Henrik Sommerfeld <henrik@sommerfeld.nu>
2019-09-30 09:34:31 +0200

    Rendering tweets

--- src/components/Tweets.tsx
+++ src/components/Tweets.tsx
@@ -1,18 +1,31 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
+import styled from 'styled-components';
+import { tailwindColors } from '../tailwind-colors';
 
 export default function Tweets() {
-  const tweets = useStaticQuery<TweetsData>(isabelsTweetsQuery);
+  const tweetsData = useStaticQuery<TweetsData>(isabelsTweetsQuery);
+  const tweets = tweetsData.allTwitterStatusesUserTimelineIsabel.nodes;
   console.log('TCL: Tweets -> tweets', tweets);
 
-  return null;
+  const toRender = tweets.map(tweet => {
+    return <div key={tweet.id}>{tweet.full_text}</div>;
+  });
+
+  return <TweetsStyled>{toRender}</TweetsStyled>;
 }
+
+const TweetsStyled = styled('div')`
+  background-color: ${tailwindColors.green600};
+`;
 
 const sampleData = {
   data: {
     allTwitterStatusesUserTimelineIsabel: {
       nodes: [
         {
+          id: 'e0d239b6ef7a21c7ff7e25481328f46b',
+          id_str: '1174533019736203265',
           full_text: 'https://t.co/DM6et4kNZP',
           retweet_count: 0,
           favorite_count: 0,
@@ -30,8 +43,15 @@ const sampleData = {
             ],
             media: null,
           },
+          user: {
+            screen_name: 'isommerfeld',
+            url: 'https://t.co/t0qRJl64cL',
+            name: 'Isabel Sommerfeld',
+          },
         },
         {
+          id: '212edf55986f662c04ed113d2af31425',
+          id_str: '1162427536510521344',
           full_text:
             'Började arbetsdagen igår med att göra ett Facebook-inlägg om ”dick pics”. Stå på er och polisanmäl dessa sexuella ofredanden. #polisen #brott #sexuelltofredande #metoo  https://t.co/XBFIgFMpAI',
           retweet_count: 0,
@@ -63,8 +83,15 @@ const sampleData = {
             ],
             media: null,
           },
+          user: {
+            screen_name: 'isommerfeld',
+            url: 'https://t.co/t0qRJl64cL',
+            name: 'Isabel Sommerfeld',
+          },
         },
         {
+          id: 'e85f61b1f5109f2b2cd2192cf89a37f6',
+          id_str: '1162016496748564480',
           full_text:
             'Polisutredaren: ”Vi är trötta på att behöva se era dickpics” https://t.co/Q67rgiTnHH',
           retweet_count: 0,
@@ -83,8 +110,15 @@ const sampleData = {
             ],
             media: null,
           },
+          user: {
+            screen_name: 'isommerfeld',
+            url: 'https://t.co/t0qRJl64cL',
+            name: 'Isabel Sommerfeld',
+          },
         },
         {
+          id: '7bb8ce57af02965dc487e3c30e28a051',
+          id_str: '1155209781168214017',
           full_text:
             'RT @broughtfromheII: Hela det här ASAP Rocky dramat kommer sluta med att USA invaderar Schweiz',
           retweet_count: 1683,
@@ -100,8 +134,15 @@ const sampleData = {
             urls: [],
             media: null,
           },
+          user: {
+            screen_name: 'isommerfeld',
+            url: 'https://t.co/t0qRJl64cL',
+            name: 'Isabel Sommerfeld',
+          },
         },
         {
+          id: 'ff0646c3ddaec8114883f6177b6b8d21',
+          id_str: '1155207847774425095',
           full_text:
             'RT @ahamrud: Hon var hatets måltavla – nu är hon död https://t.co/uOcREU5n1K via @SvD',
           retweet_count: 19,
@@ -127,6 +168,11 @@ const sampleData = {
             ],
             media: null,
           },
+          user: {
+            screen_name: 'isommerfeld',
+            url: 'https://t.co/t0qRJl64cL',
+            name: 'Isabel Sommerfeld',
+          },
         },
       ],
     },
@@ -134,10 +180,17 @@ const sampleData = {
 };
 
 interface TweetData {
+  id: string;
+  id_str: string;
   full_text: string;
   retweet_count: number;
   favorite_count: number;
   created_at: string;
+  user: {
+    screen_name: string;
+    url: string;
+    name: string;
+  };
   entities?: {
     hashtags: {
       text: string;
@@ -167,10 +220,17 @@ const isabelsTweetsQuery = graphql`
   query {
     allTwitterStatusesUserTimelineIsabel(limit: 5) {
       nodes {
+        id
+        id_str
         full_text
         retweet_count
         favorite_count
         created_at
+        user {
+          screen_name
+          url
+          name
+        }
         entities {
           hashtags {
             text
