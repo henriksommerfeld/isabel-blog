247fd9b5917dfac0272521538dc2b46b7c753158
Henrik Sommerfeld <henrik@sommerfeld.nu>
2019-10-09 09:21:45 +0200

    Extracted global styled

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,14 @@
 import React, { ReactNode } from 'react';
 import { Helmet } from 'react-helmet';
-import styled, { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 import Footer from './Footer';
 import useSiteMetadata from './SiteMetadata';
 import { withPrefix } from 'gatsby';
-import { colors, breakpoints, spacing } from '../constants';
+import { colors, breakpoints } from '../constants';
 import Header from './Header';
-import { tailwindColors } from '../tailwind-colors';
 import { SearchResult } from './SearchResult';
 import { WindowLocation } from '@reach/router';
+import { GlobalStyles } from '../global-styles';
 
 interface TemplateWrapperProps {
   children: ReactNode;
@@ -28,7 +28,7 @@ export default function TemplateWrapper({
 
   return (
     <>
-      <Helmet>
+      <Helmet defer={false}>
         <html lang="sv" />
         <title>{title}</title>
         <meta name="description" content={description || ''} />
@@ -40,8 +40,12 @@ export default function TemplateWrapper({
           property="og:image"
           content={`${withPrefix('/')}img/og-image.jpg`}
         />
+        <link
+          rel="canonical"
+          href={`http://www.isabelsommerfeld.com/${location.pathname}`}
+        />
       </Helmet>
-      <GlobalStyle />
+      <GlobalStyles />
       <Page>
         <Header location={location} />
 
@@ -75,97 +79,4 @@ const Page = styled('div')`
   background-attachment: fixed;
   background-position: top center;
   background-size: cover;
-`;
-
-export const GlobalStyle = createGlobalStyle`
-  /* Box sizing rules */
-  *,
-  *::before,
-  *::after {
-    box-sizing: border-box;
-  }
-
-  /* Remove default padding */
-  ul[class],
-  ol[class] {
-    padding: 0;
-  }
-
-
-  /* Set core body defaults */
-  body {
-    min-height: 100vh;
-    scroll-behavior: smooth;
-    text-rendering: optimizeSpeed;
-  }
-
-  /* Remove list styles on ul, ol elements with a class attribute */
-  ul[class],
-  ol[class] {
-    list-style: none;
-  }
-
-  /* Make images easier to work with */
-  img {
-    max-width: 100%;
-    display: block;
-  }
-
-  /* Inherit fonts for inputs and buttons */
-  input,
-  button,
-  textarea,
-  select {
-    font: inherit;
-  }
-
-
-  body {
-    margin: 0;
-    overflow-x: hidden;
-  }
-  body:not(.keyboard-navigation) * {
-    outline: none;
-  }
-  ::selection {
-    background-color: ${colors.selectionBackground};
-    color: ${colors.white};
-  }
-  img {
-    pointer-events: none;
-    margin-bottom: 0;
-  }
-  a {
-    color: ${colors.link};
-    transition: color, text-shadow, background 150ms ease-out, outline 60ms;
-    background: linear-gradient(
-    to right,
-    ${tailwindColors.red700} 0%,
-    ${tailwindColors.red400} 50%,
-    ${tailwindColors.red700} 100%
-    );
-    background-position-x: left;
-    background-position-y: 97%;
-    background-repeat: repeat;
-    background-size: auto;
-    background-size: 0px 2px;
-    background-repeat: no-repeat;
-  }
-  a:visited {
-    color: ${colors.linkVisited};
-  }
-  a:focus, a:active, a:hover {
-    color: ${colors.linkFocus};
-    background-size: 100% 2px;
-
-  }
-  .screen-reader-text {
-    display: none;
-  }
-  .gatsby-resp-image-background-image, .gatsby-resp-image-image, .featured-thumbnail {
-    margin: 0;
-    padding: 4px;
-    box-shadow: rgba(34, 25, 25, 0.4) 0 1px 3px !important;
-    background-color: #fff;
-  }
 `;
---
+++ src/global-styles.ts
@@ -0,0 +1,96 @@
+import { createGlobalStyle } from 'styled-components';
+import { colors } from './constants';
+import { tailwindColors } from './tailwind-colors';
+
+export const GlobalStyles = createGlobalStyle`
+  /* Box sizing rules */
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  /* Remove default padding */
+  ul[class],
+  ol[class] {
+    padding: 0;
+  }
+
+
+  /* Set core body defaults */
+  body {
+    min-height: 100vh;
+    scroll-behavior: smooth;
+    text-rendering: optimizeSpeed;
+  }
+
+  /* Remove list styles on ul, ol elements with a class attribute */
+  ul[class],
+  ol[class] {
+    list-style: none;
+  }
+
+  /* Make images easier to work with */
+  img {
+    max-width: 100%;
+    display: block;
+  }
+
+  /* Inherit fonts for inputs and buttons */
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+
+
+  body {
+    margin: 0;
+    overflow-x: hidden;
+  }
+  body:not(.keyboard-navigation) * {
+    outline: none;
+  }
+  ::selection {
+    background-color: ${colors.selectionBackground};
+    color: ${colors.white};
+  }
+  img {
+    pointer-events: none;
+    margin-bottom: 0;
+  }
+  a {
+    color: ${colors.link};
+    transition: color, text-shadow, background 150ms ease-out, outline 60ms;
+    background: linear-gradient(
+    to right,
+    ${tailwindColors.red700} 0%,
+    ${tailwindColors.red400} 50%,
+    ${tailwindColors.red700} 100%
+    );
+    background-position-x: left;
+    background-position-y: 97%;
+    background-repeat: repeat;
+    background-size: auto;
+    background-size: 0px 2px;
+    background-repeat: no-repeat;
+  }
+  a:visited {
+    color: ${colors.linkVisited};
+  }
+  a:focus, a:active, a:hover {
+    color: ${colors.linkFocus};
+    background-size: 100% 2px;
+
+  }
+  .screen-reader-text {
+    display: none;
+  }
+  .gatsby-resp-image-background-image, .gatsby-resp-image-image, .featured-thumbnail {
+    margin: 0;
+    padding: 4px;
+    box-shadow: rgba(34, 25, 25, 0.4) 0 1px 3px !important;
+    background-color: #fff;
+  }
+`;
