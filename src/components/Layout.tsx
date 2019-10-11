5e16559a0ae8ddc9f0e03e78889b7a7db9718c9d
Henrik Sommerfeld <henrik@sommerfeld.nu>
2019-10-11 10:07:17 +0200

    Changed canonical to https

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -31,7 +31,7 @@ export default function TemplateWrapper({
 }: TemplateWrapperProps) {
   const { title } = useSiteMetadata();
   const finalTitle = pageTitle ? `${pageTitle} | ${title}` : title;
-  const baseUrl = 'http://www.isabelsommerfeld.com';
+  const baseUrl = 'https://www.isabelsommerfeld.com';
   const canonical = `${baseUrl}${location.pathname}`;
   const locale = pageLanguage === 'en' ? 'en_US' : 'sv_SE';
 
