8bb3d300c528f6e3673a450a96d64f83840f76bd
Henrik Sommerfeld <henrik@sommerfeld.nu>
2020-12-01 23:38:34 +0100

    Changed subdomain for stats

--- src/html.js
+++ src/html.js
@@ -10,7 +10,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <script async defer data-domain="isabelsommerfeld.com" src="https://statistik.isabelsommerfeld.com/js/index.js"></script>
+        <script async defer data-domain="isabelsommerfeld.com" src="https://stats.isabelsommerfeld.com/js/index.js"></script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
